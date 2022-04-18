563edb9e98fe65b7c72d72090b5fa33453ecccf1
Juan-Zuniga-Codoceo <zcja.89@gmail.com>
2022-04-18 10:53:17 -0400

    Ajax

---
+++ Ajax.js
@@ -0,0 +1,26 @@
+AJAX
+
+## Que es  AJAX?
+
+AJAX son las siglas de Asynchronous JavaScript and XML. XML es raramente relevante, pero cuando desarrollamos aplicaciones web, usamos Ajax para hacer cosas asincrónicas como actualizar una página, hacer acciones, etc.
+
+En resumen, AJAX se trata de actualizar partes de una página web sin tener que recargar toda la web. Eso es muy útil si tu sitio web es grande , no querrá que tus usuarios tengan que cargar la misma información varias veces.
+
+### En que se basa Ajax ?
+
+AJAX se basa en un montón de tecnologías. No tienes que ser un experto en todas ellas pero un poco de conocimiento de fondo será útil. Esto debería darte los antecedentes suficientes para que te hagas una  idea de AJAX.
+
+### Las tecnologías que forman AJAX son
+
+- XHTML y CSS, para crear una presentación basada en estándares.
+- DOM, para la interacción y manipulación dinámica de la presentación.
+- XML, XSLT y JSON, para el intercambio y la manipulación de información.
+- XMLHttpRequest, para el intercambio asíncrono de información.
+- JavaScript, para unir todas las demás tecnologías.
+
+HTTP Methods
+
+- Get: El servidor debe buscar información en la base de datos.
+- Put: El servidor va a modificar algo en la base de datos.
+- Delete: El servidor va a ir a la base de datos y va a eliminar algo.
+- Post: El servidor va agregar información en la base de datos.
